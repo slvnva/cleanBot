9fa711127de87e80828ad33072e4fb907661e3da
slvnva00 <slvnva@tspu.edu.ru>
2024-04-16 08:23:40 +0700

    bot

---
+++ bot.py
@@ -0,0 +1,29 @@
+import telebot
+
+botTimeWeb = telebot.TeleBot('7182142694:AAHbsjrzlUTGhz45JDglxDtPoYFtKzQvtDA')
+
+from telebot import types
+
+
+@botTimeWeb.message_handler(commands=['start'])
+def startBot(message):
+    first_mess = f"<b>{message.from_user.first_name} {message.from_user.last_name}</b>, привет!\nХочешь расскажу немного о нашей компании?"
+    markup = types.InlineKeyboardMarkup()
+    button_yes = types.InlineKeyboardButton(text='Да', callback_data='yes')
+    markup.add(button_yes)
+    botTimeWeb.send_message(message.chat.id, first_mess, parse_mode='html', reply_markup=markup)
+
+
+@botTimeWeb.callback_query_handler(func=lambda call: True)
+def response(function_call):
+    if function_call.message:
+        if function_call.data == "yes":
+            second_mess = "Мы облачная платформа для разработчиков и бизнеса. Более детально можешь ознакомиться с нами на нашем сайте!"
+            markup = types.InlineKeyboardMarkup()
+            markup.add(types.InlineKeyboardButton("Перейти на сайт", url="https://timeweb.cloud/"))
+            botTimeWeb.send_message(function_call.message.chat.id, second_mess, reply_markup=markup)
+            botTimeWeb.answer_callback_query(function_call.id)
+
+
+botTimeWeb.infinity_polling()
+# https://timeweb.cloud/tutorials/python/kak-sozdat-telegram-bota-na-python
